1511fbd288c95c502cd80242f3a50addf90c352c
GallupinGallup <gallupingallup@gmail.com>
2016-11-02 13:23:10 -0600

    Henrichesn made a mental flu and used the computed property structure for get/set instead of correct version

--- src/graph/view/FirstPanel.java
+++ src/graph/view/FirstPanel.java
@@ -1,10 +1,10 @@
 package graph.view;
 
 import java.awt.Color;
-import javax.swing.JButton;
-import javax.swing.SpringLayout;
-import javax.swing.JPanel;
+import javax.swing.*;
 import graph.controller.GraphController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstPanel extends JPanel
 {
@@ -49,7 +49,27 @@ public class FirstPanel extends JPanel
 	}
 	private void setupListeners()
 	{
-		
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.MAGENTA);
+			}
+		});
+		randomButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeBackgroundColor();
+			}
+		});
 	}
-	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		this.setBackground(new Color(red, green, blue));
+	}
 }
+
